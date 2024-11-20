6dfbc6fa8fbfec8410cdf4f5d13a13f2b6f93256
Gabriel Pacheco <gabrielpacheco.wddigital@gmail.com>
2024-11-20 11:14:33 -0300

    feat: create http cliente and custom class error

---
+++ src/services/utils/HttpClient.js
@@ -0,0 +1,38 @@
+import delay from '../../utils/delay';
+import APIError from '../../errors/APIError';
+
+class HttpClient {
+  constructor(baseURL) {
+    this.baseURL = baseURL;
+  }
+
+  async get(path) {
+    const response = await fetch(`${this.baseURL}${path}`);
+    await delay(500);
+
+    let body = null;
+    const contentType = response.headers.get('Content-Type');
+    if (contentType.includes('application/json')) {
+      body = await response.json();
+    }
+
+    if (response.ok) {
+      return body;
+    }
+
+    throw new APIError(response, body);
+  }
+
+  async post(path, data) {
+    const response = await fetch(`${this.baseURL}${path}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    return response.json();
+  }
+}
+
+export default HttpClient;
